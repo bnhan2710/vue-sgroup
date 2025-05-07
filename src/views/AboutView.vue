fc2b13c0bfa3c4bb94d773f986d403481211d1c2
Huỳnh Như Bảo Nhân <baonhannice@gmail.com>
2025-05-07 16:53:22 +0700

    set up project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about p-8 bg-gray-100 rounded-lg shadow-md">
+    <h1 class="text-3xl font-bold text-blue-600 mb-4">This is an about page</h1>
+    <p class="text-gray-700">This page is now styled with Tailwind CSS!</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    AutoImport({
+      dts: true,
+      imports: ['vue', 'vue-router', 'pinia'],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
